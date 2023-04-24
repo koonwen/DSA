635c74bcde25f32f2b97fefc88af7a1fc52bb03e
koonwen <koonwen@gmail.com>
2023-04-24 12:19:36 +0800

    Delete old

--- src/Searching/BinarySearch/binsearch.ml
+++
@@ -1,8 +0,0 @@
-let rec binary_search element arr startIndex endIndex =
-	if startIndex > endIndex
-	then -1
-	else let middleIndex = (startIndex + endIndex) / 2 in
-		 if arr[middleIndex] = element then middleIndex 
-		 else if element < arr[middleIndex] 
-		 then binary_search element arr middleIndex endIndex
-		 else binary_search element arr startIndex middleIndex 
--- src/Sorting/utils.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-def list_generator(size = 10):
-	return [randint(0,1000) for x in range(size)]
-
-if __name__ == "__main__":
-	result = list_generator()
-	print(result)
